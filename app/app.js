6aae54cfb8df3499dee63e5923dda545b102e8d7
nicwands <nwands@ithaca.edu>
2020-04-01 11:29:32 -0600

    updated security features

--- app/app.js
+++ app/app.js
@@ -1,16 +1,26 @@
-import createError from 'http-errors';
 import express from 'express';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import createError from 'http-errors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
+import RateLimit from 'express-rate-limit';
+
+let limiter = new RateLimit({
+	windowMs: 15*60*100,
+	max: 100,
+	delayMs: 0,
+	message: 'Too many requests, please try again later'
+});
 
 import routes from './routes';
 
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,6 +37,7 @@ app.use(cors({
 	credentials: true
 }));
 app.use(fileUpload());
+app.use(limiter);
 
 // Hook up all routes
 app.use('/', routes);
