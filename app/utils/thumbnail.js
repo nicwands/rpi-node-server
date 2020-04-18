e0f0f9fba7011e682407a043dac72e9a46871b24
nicwands <nwands@ithaca.edu>
2020-04-18 16:11:07 -0600

    fixed thumbnail lag

--- app/utils/thumbnail.js
+++ app/utils/thumbnail.js
@@ -2,15 +2,16 @@ import fs from 'fs';
 import appRoot from 'app-root-path';
 import imageThumbnail from 'image-thumbnail';
 
-const createThumbnail = (buffer, name) => {
+const createThumbnail = async (buffer, name, size) => {
     const options = {
-        percentage: 25
+        percentage: (size > 2000000) ? 10 : 25
     };
     imageThumbnail(buffer, options)
         .then(thumbnail => {
             fs.writeFileSync(appRoot + '/uploads/thumbnails/THUMB-' + name, thumbnail);
         })
         .catch(err => console.error(err));
+    await imageThumbnail
 };
 
 export default createThumbnail;
